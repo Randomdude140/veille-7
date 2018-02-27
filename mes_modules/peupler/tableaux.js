5e590107155473ad9cb2fd56d75e4f18d5585ae5
Arnaud Voyer <ArnaudRandomdudeVoyer@gmail.com>
2018-02-27 15:31:29 -0500

    Remise de l'exercice 7

---
+++ mes_modules/peupler/tableaux.js
@@ -0,0 +1,57 @@
+"use strict";
+const tabNom = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9"
+]
+
+const tabPrenom = [
+    "Caitlin",
+    "Kaytelan",
+    "Catlin",
+    "Katelyn",
+    "Cayetlyn",
+    "Caitlinn",
+    "Kateland",
+    "Kaytelynn",
+    "Catlyn"
+]
+
+const tabTele = [
+    "462-641-5945",
+    "672-089-4735",
+    "568-168-5493",
+    "729-492-4524",
+    "547-489-4681",
+    "142-546-4852",
+    "123-456-7890",
+    "486-485-6295",
+    "514-697-5824"
+]
+
+const tabEmail = [
+    "caitlin@kate.com",
+    "kaytelan@kate,com",
+    "catlin@kate.com",
+    "katelyn@kate.com",
+    "cayetlyn@kate.com",
+    "caitlinn@kate.com",
+    "kateland@kate.com",
+    "kaytelynn@kate.com",
+    "catlyn@kate.com"
+]
+
+const tableau = {
+    tabNom : tabNom,
+    tabPrenom : tabPrenom,
+    tabTele : tabTele,
+    tabEmail : tabEmail
+}
+
+module.exports = tableau
